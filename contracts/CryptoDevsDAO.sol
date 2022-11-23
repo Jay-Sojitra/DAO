c4d30a4b2f95806c0ce39038c35f3880617571f0
Jay-Sojitra <84234572+Jay-Sojitra@users.noreply.github.com>
2022-11-23 18:25:40 +0530

    first commit

---
+++ contracts/CryptoDevsDAO.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract CryptoDevsDAO is Ownable {
+    // We will write contract code here
+}
